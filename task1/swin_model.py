4b103e32b064b3be3888daaeefc9f8234d0a2df0
Advik Sinha <advsinha112@gmail.com>
2023-10-17 21:01:42 +0530

    feat: add swin transformer related layers and helper functions

---
+++ task1/swin_model.py
@@ -0,0 +1,221 @@
+import tensorflow as tf
+import os
+from swin_utils import window_partition, window_reverse
+
+os.environ['TF_CPP_MIN_LG_LEVEL'] = "2"
+
+class PatchPartition(tf.keras.layers.Layer):
+
+    def __init__(self, patch_size = 4):
+        super(PatchPartition, self).__init__();
+        self.patch_size = patch_size
+
+    def call(self, input):
+        B, _, _, C = input.shape
+        patches = tf.image.extract_patches(
+            images = input,
+            sizes = [1, self.patch_size, self.patch_size, 1],
+            strides = [1, self.patch_size, self.patch_size, 1],
+            rates = [1, 1, 1, 1],
+            padding = "VALID"
+        )
+        patch_dim = self.patch_size * self.patch_size * C
+        patches = tf.reshape(patches, [B, -1, patch_dim])
+        return patches
+ 
+
+class PatchEmbedding(tf.keras.layers.Layer):
+
+    def __init__(self, embed_dim = 96, norm_layer = None):
+        super(PatchEmbedding, self).__init__()
+        self.embed_dim = embed_dim
+        self.proj = tf.keras.layers.Dense(embed_dim)
+        self.norm = norm_layer
+
+
+    def call(self, input):
+        x = self.proj(input)
+        if self.norm is not None:
+            x = self.norm(x)
+        return x
+
+
+class PatchMerging(tf.keras.layers.Layer):
+
+    def __init__(self, input_res, embed_dim):
+        super(PatchMerging, self).__init__()
+        self.input_res = input_res
+        self.embed_dim = embed_dim
+        self.reduction = tf.keras.layers.Dense(2 * embed_dim, use_bias = False)
+        self.norm_layer = tf.keras.layers.LayerNormalization()
+
+    def call(self, input):
+        H, W = self.input_res
+        B, _ , C = input.shape
+        x = tf.reshape(input, [B, H, W, C])
+        x0 = x[:, 0::2, 0::2, :]
+        x1 = x[:, 1::2, 0::2, :]
+        x2 = x[:, 0::2, 1::2, :]
+        x3 = x[:, 1::2, 1::2, :]
+        x = tf.concat([x0, x1, x2, x3], axis = -1)
+        x = tf.reshape(x, [B, -1, 4 * C])
+        x = self.norm_layer(x)
+        x = self.reduction(x)
+        
+        return x
+    
+class WindowAttention(tf.keras.layers.Layer):
+
+    def __init__(self, 
+                embed_dim, 
+                window_size, 
+                num_heads, 
+                attn_dropout_rate = 0.,
+                proj_dropout_rate = 0.):
+        super(WindowAttention, self).__init__()
+        self.embed_dim = embed_dim
+        self.window_size = window_size
+        self.num_heads = num_heads
+        head_dim = self.embed_dim // num_heads
+        self.scale = head_dim ** -0.5
+
+        self.rel_pos_table = self.add_weight(shape = ((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads),
+                                                           initializer = tf.keras.initializers.Zeros(),
+                                                           trainable = True)
+        coords_h = tf.range(self.window_size[0])
+        coords_w = tf.range(self.window_size[1])
+        coords = tf.stack(tf.meshgrid(coords_h, coords_w, indexing = 'ij'))
+        coords = tf.reshape(coords, (2, -1))
+        rel_coords = coords[:, :, None] - coords[:, None, :]
+        rel_coords = tf.transpose(rel_coords, [1, 2, 0])
+        rel_coords = rel_coords.numpy()
+        rel_coords[:, :, 0] += self.window_size[0] - 1  
+        rel_coords[:, :, 1] += self.window_size[1] - 1
+        rel_coords[:, :, 0] *= 2 * self.window_size[1] - 1
+        rel_pos_index = rel_coords.sum(-1)
+        self.rel_pos_index = tf.Variable(
+            initial_value = tf.convert_to_tensor(rel_pos_index), trainable = False
+        )  
+        self.qkv = tf.keras.layers.Dense(3 * embed_dim, use_bias=True)
+        self.projection = tf.keras.layers.Dense(embed_dim)  
+        self.attn_Dropout = tf.keras.layers.Dropout(attn_dropout_rate) 
+        self.proj_Dropout = tf.keras.layers.Dropout(proj_dropout_rate)
+
+
+    def call(self, input, mask = None):
+        num_batch_windows, num_patches, C = input.shape
+        qkv = self.qkv(input)
+        qkv = tf.reshape(qkv, [3, num_batch_windows, self.num_heads, num_patches, C // self.num_heads])
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        q *= self.scale
+        attn = tf.matmul(q, tf.transpose(k, [0, 1, 3, 2]))
+        rel_pos_bias = tf.gather(self.rel_pos_table, tf.reshape(self.rel_pos_index, [-1]))
+        rel_pos_bias = tf.reshape(rel_pos_bias, [self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1])
+        rel_pos_bias = tf.transpose(rel_pos_bias, [2, 0 ,1])
+        rel_pos_bias = tf.expand_dims(rel_pos_bias, axis = 0)
+        attn += rel_pos_bias
+        if mask:
+            num_windows = mask.shape[0]
+            attn = tf.reshape(attn, [num_batch_windows // num_windows, num_windows, self.num_heads, num_patches, num_patches]) + tf.expand_dims(tf.expand_dims(mask, 1), 0)
+            attn = tf.reshape(attn, [-1, self.num_heads, num_patches, num_patches])
+            attn = tf.keras.activations.softmax(attn)
+        else:
+            attn = tf.keras.activations.softmax(attn)
+
+        attn = self.attn_Dropout(attn)
+        attn = tf.transpose((attn @ v), [0, 2, 1, 3])
+        attn = tf.reshape(attn, [num_batch_windows, num_patches, C])
+        x = self.projection(attn)
+        x = self.proj_Dropout(x)
+        return x
+
+
+class TransformerBlock(tf.keras.layers.Layer):
+
+    def __init__(self, 
+                embed_dim, 
+                input_res,
+                num_heads, 
+                mlp_units = 1024,
+                window_size = 7, 
+                window_shift = 0, 
+                attn_drop = 0.,
+                proj_drop = 0.,
+                ):
+        
+        super(TransformerBlock, self).__init__()
+        self.embed_dim = embed_dim
+        self.input_res = input_res
+        self.num_heads = num_heads
+        self.window_size = window_size
+        self.window_shift = window_shift
+
+
+        self.norm_layer1 = tf.keras.layers.LayerNormalization()
+        self.norm_layer2 = tf.keras.layers.LayerNormalization()
+        self.attn = WindowAttention(embed_dim, (self.window_size, self.window_size), num_heads, attn_drop, proj_drop)
+        self.Mlp = tf.keras.Sequential([
+            tf.keras.layers.Dense(mlp_units),
+            tf.keras.layers.Activation(tf.keras.activations.gelu),
+            tf.keras.layers.Dropout(proj_drop),
+            tf.keras.layers.Dense(embed_dim),
+            tf.keras.layers.Dropout(proj_drop)
+        ])
+        if self.window_shift > 0:
+            height, width = self.input_res
+            mask = tf.zeros((1, height, width, 1))
+            h_slices = (
+                slice(0, -self.window_size),
+                slice(-self.window_size, -self.window_shift),
+                slice(-self.window_shift, None),
+            )
+            w_slices = (
+                slice(0, -self.window_size),
+                slice(-self.window_size, -self.window_shift),
+                slice(-self.window_shift, None),
+            )
+            count = 0
+            for h in h_slices: 
+                for w in w_slices:
+                    mask[:, h, w, :] = count
+                    count += 1
+            mask_windows = window_partition(mask, self.window_size)
+            mask_windows = tf.reshape(mask_windows, [-1, self.window_size * self.window_size])
+            mask = tf.expand_dims(mask_windows, 1) - tf.expand_dims(mask_windows, 2)
+            mask = tf.where(mask != 0, -100.0, mask)
+            mask = tf.where(mask == 0, 0.0, mask)
+            self.mask = tf.Variable(initial_value=mask, trainable=False)
+        else:
+            self.mask = None
+
+    def call(self, input):
+        height, width = self.input_res
+        B, num_patches, C = input.shape
+        skip_connection = input
+        x = self.norm_layer1(input)
+        x = tf.reshape(x, [B, height, width, C])
+        if self.window_shift > 0:
+            shifted_x = tf.roll(
+                x, shift = [-self.window_shift, self.window_shift], axis = [1, 2]
+            )
+        else:
+            shifted_x = x
+
+        partitioned_windows = window_partition(shifted_x, self.window_size)
+        partitioned_windows = tf.reshape(partitioned_windows, [-1, self.window_size * self.window_size, C])
+        attn_windows = self.attn(partitioned_windows, self.mask)
+        attn_windows = tf.reshape(attn_windows, [-1, self.window_size, self.window_size, C])
+        x = window_reverse(attn_windows,  self.window_size, height, width)
+        if self.window_shift > 0:
+            x = tf.roll(
+                x, shift = [self.window_shift, self.window_shift], axis = [1, 2]
+            )
+        x = tf.reshape(x, [B, num_patches, C])
+        x += skip_connection
+        skip_connection = x
+        x = self.norm_layer2(x)
+        x = self.Mlp(x)
+        x += skip_connection
+        return x
+
+
---
+++ task1/swin_utils.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+def window_partition(x, window_size):
+    B, H, W, C = x.shape
+    x = tf.reshape(x, [B, H // window_size, window_size, W // window_size, window_size, C])
+    windows = tf.transpose(x, [0, 1, 2, 3, 4, 5])
+    windows = tf.reshape(windows, [-1, window_size, window_size, C])
+    return windows
+
+
+def window_reverse(windows, window_size, H, W):
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = tf.reshape(windows, [B, H // window_size, W // window_size, window_size, window_size, -1])
+    x = tf.transpose(x, [0, 1, 3, 2, 4, 5])
+    x = tf.reshape(x, [B, H, W, -1])
+    return x
+
