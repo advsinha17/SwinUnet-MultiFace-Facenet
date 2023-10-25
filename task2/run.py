7edf36b9d4f1d07c975d79adab61bc782578b53f
Advik Sinha <advsinha112@gmail.com>
2023-10-25 20:46:53 +0530

    refactor: change inputs structure

--- task2/run.py
+++ task2/run.py
@@ -14,36 +14,39 @@ train_parser.add_argument('--batch_size', type=int, default=64,
                             help='Number of training images per batch. Defaults to 64.')
 train_parser.add_argument('--freeze_layers', type=int, default=250,
                             help='Number of layers of Facenet model to freeze. Defaults to 250.')
-train_parser.add_argument('--load_pretrained_weights', type=bool, default=True,
-                            help='''Whether to load pretrained Facenet weights. If both load_pretrained_weights 
-                            and load_finetuned_weights are True, then finetuned weights will be loaded. Defaults to True.''')
-train_parser.add_argument('--load_trained_weights', type=bool, default=False,
-                            help='''Whether to load trained/finetuned weights (if exists) 
-                            from previous trainings. If both load_pretrained_weights 
-                            and load_trained_weights are True, then trained/finetuned weights will be loaded.
+train_parser.add_argument('--load_pretrained_weights', action='store_true', 
+                          help='Load pretrained Facenet weights.')
+train_parser.add_argument('--no-load_pretrained_weights', dest='load_pretrained_weights', action='store_false',
+                          help='Do not load pretrained Facenet weights.')
+train_parser.add_argument('--load_trained_weights', action='store_true',
+                          help='''Whether to load trained/finetuned weights (if exists) 
+                            from previous trainings. If both --load_pretrained_weights 
+                            and --load_trained_weights are True, then trained/finetuned weights will be loaded.
                             If model has not been trained or trained/finetuned weights are not available, pretrained weights will 
-                            be loaded based on value of --load_pretrained_weights argument. Defaults to False''')
+                            be loaded based on value of --load_pretrained_weights argument. Trained weights will only be available in the `weights` folder. False if not given.''')
 
+train_parser.set_defaults(load_pretrained_weights=True, load_trained_weights=False)
 
 # Inference arguments
 inference_parser = subparsers.add_parser('inference')
 inference_parser.add_argument('--input_folder', type=str, default="input_images/",
-                                help='''Path to the folder containing images for inference. Requires
-                                absolute path or path relative to `run.py` script. Raises FileNotFoundError if directory does
-                                not exist. Defaults to `input_images/`''')
+                                help='''Path to the folder containing images for inference. Directory must exist. Defaults to `input_images/`''')
 inference_parser.add_argument('--output_folder', type=str, default="output_images/",
                                 help='''Output folder path. Subdirectories will be created for each person
-                                in the images. Requires absolute path or path relative to `run.py` script. An existing directory with the same 
+                                in the images. An existing directory with the same 
                                 path will be overwritten. Defaults to `output_images/`''')
-inference_parser.add_argument('--use_pretrained_weights', type=bool, default=True,
-                            help='''Whether to use pretrained Facenet weights. If both use_pretrained_weights 
-                            and use_finetuned_weights are True, then finetuned weights will be used. Defaults to True.''')
-inference_parser.add_argument('--use_trained_weights', type=bool, default=False,
-                            help='''Whether to use trained/finetuned weights (if exists) 
-                            from training the model. If both use_pretrained_weights 
-                            and use_trained_weights are True, then trained/finetuned weights will be used.
+inference_parser.add_argument('--use_pretrained_weights', action='store_true', 
+                          help='Load pretrained Facenet weights.')
+inference_parser.add_argument('--no-use_pretrained_weights', dest='load_pretrained_weights', action='store_false',
+                          help='Do not load pretrained Facenet weights.')
+inference_parser.add_argument('--use_trained_weights', action='store_true',
+                          help='''Whether to use trained/finetuned weights (if exists) 
+                            from previous trainings. If both --load_pretrained_weights 
+                            and --load_trained_weights are True, then trained/finetuned weights will be used.
                             If model has not been trained or trained/finetuned weights are not available, pretrained weights will 
-                            be used. Defaults to False''')
+                            be loaded. Trained weights will only be available in the `weights` folder. False if not given.''')
+
+inference_parser.set_defaults(use_pretrained_weights=True, use_trained_weights=False)
 
 args = parser.parse_args()
 
