dfaab4c8eab1eb2dfeea2d04dde3370f06fd9329
Advik Sinha <advsinha112@gmail.com>
2023-10-27 02:01:34 +0530

    feat: function to train the model

---
+++ task1/task1.1/train.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+from model import BetaVAE
+from dataset import DataGenerator
+from tqdm import tqdm
+
+
+def train(model, epochs, data, optimizer = tf.keras.optimizers.Adam()):
+
+    model.compile(optimizer=optimizer)
+
+    for epoch in range(epochs):
+        progress_bar = tqdm(data, total=len(data_generator), unit="batch")
+        for batch in progress_bar:
+            losses  = model.train_step(batch)
+            progress_bar.set_description(f"Loss: {losses['loss'].numpy():.4f}, Reconstruction Loss: {losses['reconstruction_loss'].numpy():.4f}, kl_div_loss: {losses['kl_loss'].numpy():.4f}")
+        print(f"End of Epoch {epoch + 1}, Loss: {losses['loss'].numpy():.4f}, Reconstruction Loss: {losses['reconstruction_loss'].numpy():.4f}, kl_div_loss: {losses['kl_loss'].numpy():.4f}")
+
+
+
+if __name__ == "__main__":
+    data_generator = DataGenerator()
+    epochs = 20
+    model = BetaVAE(10, out_channels=1, beta=4.0)
+    train(model, epochs, data_generator)
